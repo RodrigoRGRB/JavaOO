4f0468358fecd5441b89484f069dfec8d78615e2
Rodrigo Gabriel <rodrigorgrb@gmail.com>
2019-05-02 19:56:58 +0000

    Adicionando o criaConta com o metodo main

---
+++ BancoStay/CriaConta.java
@@ -0,0 +1,12 @@
+public class CriaConta{
+    public static void main(String[] args){
+      Conta primeiraConta =  new Conta();
+      primeiraConta.saldo = 200;
+
+      System.out.println(primeiraConta.saldo);
+
+      Conta segundaConta = new Conta();
+      segundaConta.titular = "Rodrigo";
+      System.out.println(segundaConta.titular);
+    }
+}
